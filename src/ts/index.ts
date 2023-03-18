c90813995468cf762dd419b963f3da988a6c386a
Itta Kato <itta.kato17@gmail.com>
2023-03-18 12:39:31 +0900

    add mobile-nav logic

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,24 @@
 import '../styles/styles.scss';
 import './gradient';
+
+const hamburgerButton = document.querySelector('.hamburger-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileNavExitButton = document.querySelector('.mobile-navbar-exit-btn');
+
+function openMobileNavigation(): void {
+  mobileNav?.classList.add('openMobileNav');
+}
+
+function closeMobileNavigation(): void {
+  mobileNav?.classList.remove('openMobileNav');
+}
+
+hamburgerButton?.addEventListener('touchstart', openMobileNavigation);
+hamburgerButton?.addEventListener('touchend', openMobileNavigation);
+hamburgerButton?.addEventListener('touchcancel', openMobileNavigation);
+hamburgerButton?.addEventListener('click', openMobileNavigation);
+
+mobileNavExitButton?.addEventListener('touchstart', closeMobileNavigation);
+mobileNavExitButton?.addEventListener('touchend', closeMobileNavigation);
+mobileNavExitButton?.addEventListener('touchcancel', closeMobileNavigation);
+mobileNavExitButton?.addEventListener('click', closeMobileNavigation);
